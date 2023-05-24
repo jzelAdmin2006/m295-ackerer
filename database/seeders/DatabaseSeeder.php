c5a8c5c6a5b238363f09820f39e933a8b32dc621
jzelAdmin2006 <jo.zeller@protonmail.com>
2023-05-24 09:09:24 +0200

    provide farm example seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,18 +13,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Plant::create([
+        $tomato = Plant::create([
             'name' => 'Tomate',
             'slug' => 'tomate',
             'description' => 'Tomaten sind Früchte der Tomatenpflanze.',
             'stock' => '10'
         ]);
 
-        Plant::create([
+        $potato = Plant::create([
             'name' => 'Kartoffel',
             'slug' => 'kartoffel',
             'description' => 'Die Kartoffel ist eine Pflanzenart aus der Gattung der Nachtschattengewächse (Solanaceae).',
             'stock' => '20'
+        ]);
+
+        Farm::create([
+            'name' => 'Acker 1',
+            'slug' => 'acker-1',
+            'plant_id' => $tomato->id,
+            'description' => 'Acker am linken Seeufer',
+            'address' => 'Seestrasse 1',
+            'city' => 'Luzern',
+            'zip' => '6003'
+        ]);
+
+        Farm::create([
+            'name' => 'Acker 2',
+            'slug' => 'acker-2',
+            'plant_id' => $potato->id,
+            'description' => 'Acker am rechten Seeufer',
+            'address' => 'Luzernerweg 2',
+            'city' => 'Luzern',
+            'zip' => '6003'
         ]);
     }
 }
